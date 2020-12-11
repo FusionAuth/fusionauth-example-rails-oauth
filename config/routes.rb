5a2c388e7cd6de256ff991118195490dcae71072
Brett Guy <brett@fusionauth.io>
2020-12-11 13:39:49 -0700

    Updated routes to match controller refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-  get '/oauth2-callback', to: 'oauth#oauth_callback'
-  get '/logout', to: 'oauth#logout'
-  get '/login', to: 'oauth#login'
+  get '/oauth2-callback', to: 'o_auth#oauth_callback'
+  get '/logout', to: 'o_auth#logout'
+  get '/login', to: 'o_auth#login'
 end
